6032a0ac4ee3c5a550a5147412be0f25278c4974
Git Automerge <jcguarinpenaranda@gmail.com>
2017-12-11 14:33:16 -0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-export class MyModule {
-  
+import * as Pubsub from 'pubsub-js';
+import * as Microcache from 'microcache';
+
+const cache = new Microcache();
+
+export class PubsubReplay {
+  static publish(message: string, data: any): boolean {
+    cache.set(message, data);
+    return Pubsub.publish(message, data);
+  }
+
+  static subscribe(message: string, func: Function, replay: boolean = false) {
+    // replay previous events
+    if (replay) {
+      const data = cache.get(message);
+      if (data) {
+        func(message, data);
+      }
+    }
+
+    // subscribe the client to pubsub
+    return Pubsub.subscribe(message, func);
+  }
+
+  static unsubscribe(message: string) {
+    Pubsub.unsubscribe(message);
+  }
+
+  static clearAllSubscriptions() {
+    Pubsub.clearAllSubscriptions();
+  }
 }
