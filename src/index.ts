e9c35d6cf8770b98915ef06846f70ecdfa48c2de
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@iMac-de-Juan.local>
2018-10-04 16:33:53 -0500

    documentation enhancement

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,49 @@ import * as Microcache from 'microcache';
 const cache = new Microcache();
 
 export class PubsubReplay {
-  static publish(message: string, data: any): boolean {
-    cache.set(message, data);
-    return Pubsub.publish(message, data);
+  /**
+   * Publishes content to a topic
+   * @param topic 
+   * @param data 
+   */
+  static publish(topic: string, data: any): boolean {
+    cache.set(topic, data);
+    return Pubsub.publish(topic, data);
   }
 
-  static subscribe(message: string, func: Function, replay: boolean = false) {
+  /**
+   * Subscribes to a topic
+   * @param topic The topic to subscribe to
+   * @param func The function to execute when new events are received
+   * @param replay Wether or not to call the function with a previously received event
+   * @return The subscription token, that may be canceled calling `unsubscribe`
+   */
+  static subscribe(topic: string, func: Function, replay: boolean = false): string {
     // replay previous events
     if (replay) {
-      const data = cache.get(message);
+      const data = cache.get(topic);
       if (data) {
-        func(message, data);
+        func(topic, data);
       }
     }
 
     // subscribe the client to pubsub
-    return Pubsub.subscribe(message, func);
+    return Pubsub.subscribe(topic, func);
   }
 
-  static unsubscribe(message: string) {
-    return Pubsub.unsubscribe(message);
+  /**
+   * Cancels all further notifications to the topic or clears all 
+   * further notifications for the function that returned the token
+   * @param tokenOrTopic The token received from `subscribe`, or the name of the topic
+   */
+  static unsubscribe(tokenOrTopic: string): void {
+    return Pubsub.unsubscribe(tokenOrTopic);
   }
 
-  static clearAllSubscriptions() {
+  /**
+   * Clears all subscriptions calling `pubsub-js`'s clearAllSubscriptions function
+   */
+  static clearAllSubscriptions(): void {
     return Pubsub.clearAllSubscriptions();
   }
 }
